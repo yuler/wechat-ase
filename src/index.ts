9a84eb2bc88408b26619b49e9d48525990cf1420
yuler <joyful4yu@gmail.com>
2020-02-27 13:32:27 +0800

    Format js code style by lint

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,55 @@
-import { randomBytes, createDecipheriv, createCipheriv } from 'crypto'
+import { randomBytes, createDecipheriv, createCipheriv } from 'crypto';
 
 const randomStringLength16 = () => {
-  return randomBytes(8).toString('hex')
-}
+  return randomBytes(8).toString('hex');
+};
 
 const pcks7Encode = (buf: Buffer) => {
-  const padLen = 32 - buf.length % 32 || 32
-  const padBuf = Buffer
-    .alloc(buf.length + padLen)
-    .fill(padLen)
-  buf.copy(padBuf)
-  return padBuf
-}
+  const padLen = 32 - (buf.length % 32) || 32;
+  const padBuf = Buffer.alloc(buf.length + padLen).fill(padLen);
+  buf.copy(padBuf);
+  return padBuf;
+};
 
 const pcks7Decode = (buf: Buffer) => {
-  let padLen = buf[buf.length - 1]
+  let padLen = buf[buf.length - 1];
   if (padLen < 1 || padLen > 32) {
-    padLen = 0
+    padLen = 0;
   }
-  return buf.slice(0, buf.length - padLen)
-}
+  return buf.slice(0, buf.length - padLen);
+};
 
 // Todo add validate msgSignature
 
 export const encode = (text: string, encodingAESKey: string) => {
-  const AESKey = Buffer.from(encodingAESKey + '=', 'base64')
-  const IV = AESKey.slice(0, 16)
-  const cipher = createCipheriv('aes-256-cbc', AESKey, IV)
-  cipher.setAutoPadding(false)
-  const textBuffer = Buffer.from(text)
+  const AESKey = Buffer.from(encodingAESKey + '=', 'base64');
+  const IV = AESKey.slice(0, 16);
+  const cipher = createCipheriv('aes-256-cbc', AESKey, IV);
+  cipher.setAutoPadding(false);
+  const textBuffer = Buffer.from(text);
   const data = Buffer.concat([
     Buffer.from(randomStringLength16()),
     Buffer.from(textBuffer.length.toString(16).padStart(8, '0'), 'hex'),
-    textBuffer
-  ])
+    textBuffer,
+  ]);
   return Buffer.concat([
     cipher.update(pcks7Encode(data)),
-    cipher.final()
-  ]).toString('base64')
-}
+    cipher.final(),
+  ]).toString('base64');
+};
 
 export const decode = (text: string, encodingAESKey: string) => {
-  const AESKey = Buffer.from(encodingAESKey + '=', 'base64')
-  const IV = AESKey.slice(0, 16)
-  const decipher = createDecipheriv('aes-256-cbc', AESKey, IV)
-  decipher.setAutoPadding(false)
+  const AESKey = Buffer.from(encodingAESKey + '=', 'base64');
+  const IV = AESKey.slice(0, 16);
+  const decipher = createDecipheriv('aes-256-cbc', AESKey, IV);
+  decipher.setAutoPadding(false);
   const decipheredBuff = pcks7Decode(
-    Buffer.concat([
-      decipher.update(text, 'base64'),
-      decipher.final()
-    ])
-  )
-  const messageLenngth = decipheredBuff.slice(16).slice(0, 4).readUInt32BE(0)
-  const content = decipheredBuff.slice(20, messageLenngth + 20)
-  return content.toString()
-}
+    Buffer.concat([decipher.update(text, 'base64'), decipher.final()])
+  );
+  const messageLenngth = decipheredBuff
+    .slice(16)
+    .slice(0, 4)
+    .readUInt32BE(0);
+  const content = decipheredBuff.slice(20, messageLenngth + 20);
+  return content.toString();
+};
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,7 +2,7 @@ import { encode, decode } from '../src';
 
 describe('weixin aes', () => {
   // const token = 'shttest'
-  const encodingAESKey = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG'
+  const encodingAESKey = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG';
   it('encode', () => {
     const content = JSON.stringify({
       ToUserName: 'gh_0052f3068328',
@@ -10,14 +10,15 @@ describe('weixin aes', () => {
       CreateTime: 1582701510,
       MsgType: 'text',
       Content: '1',
-      MsgId: 22658835000760200
-    })
-    const encrypt = encode(content, encodingAESKey)
-    expect(decode(encrypt, encodingAESKey)).toEqual(content)
-  })
+      MsgId: 22658835000760200,
+    });
+    const encrypt = encode(content, encodingAESKey);
+    expect(decode(encrypt, encodingAESKey)).toEqual(content);
+  });
 
   it('decode', () => {
-    const encrypt = 'podI1mIDNVcCql4GYcKZrM0V/KB3Y7s3k4zQCT9Apqc/IXAWxiT03mlKDSviIdp37Edlmo1QGxsIuHOiE2Pxxay2IUx/rexBTv0R6FRYI23GCbAKWnCN2p0qE+UBoHSiLSJ/Rjedk0QOfAVmC+nSCV8vqWJ7EHvL+XaX840hRNktnqe+/yp4U+F0FUHXVmNJeJq3N5ynci+1CnoKc+c6yd8hh+924cQjUHRdVnk7+DTQ/u7hyW+5SKFakBfcci47UJ1DMPmkHDE3rGcl+3pO9QCZZrcsph71EUUXxOR0lHM='
-    expect(decode(encrypt, encodingAESKey)).toMatchSnapshot()
+    const encrypt =
+      'podI1mIDNVcCql4GYcKZrM0V/KB3Y7s3k4zQCT9Apqc/IXAWxiT03mlKDSviIdp37Edlmo1QGxsIuHOiE2Pxxay2IUx/rexBTv0R6FRYI23GCbAKWnCN2p0qE+UBoHSiLSJ/Rjedk0QOfAVmC+nSCV8vqWJ7EHvL+XaX840hRNktnqe+/yp4U+F0FUHXVmNJeJq3N5ynci+1CnoKc+c6yd8hh+924cQjUHRdVnk7+DTQ/u7hyW+5SKFakBfcci47UJ1DMPmkHDE3rGcl+3pO9QCZZrcsph71EUUXxOR0lHM=';
+    expect(decode(encrypt, encodingAESKey)).toMatchSnapshot();
   });
 });
